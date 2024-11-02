cd70a7543e961b4dfa7ee8aed85680ad0ad09b22
Aarushi Singh <110608667+aarushisingh04@users.noreply.github.com>
2024-11-02 21:14:59 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,28 +1,34 @@
 from data_preprocessing import load_data, preprocess_data
-from training import train_gan, train_vae
+from training import train_gan
 from utils import generate_volatility_surface, evaluate_model
 import matplotlib.pyplot as plt
-
+import torch
+from GAN import Generator
+from data_preprocessing import load_data, preprocess_data
+from utils import plot_results, load_model
 
 data = load_data('data/vix_data.csv')
+
+print("Loaded data columns:", data.columns)
+
 processed_data = preprocess_data(data)
+
 gan_model = train_gan(processed_data)
-vae_model = train_vae(processed_data)
 
-volatility_surface = generate_volatility_surface(gan_model)
+volatility_surface = generate_volatility_surface(gan_model, processed_data)
 
+volatility_surface_np = volatility_surface.detach().numpy()
 
 
 plt.figure(figsize=(10, 6))
-plt.imshow(volatility_surface, cmap='viridis')
+plt.imshow(volatility_surface_np, cmap='viridis')
 plt.colorbar(label='Volatility')
 plt.title('Generated Volatility Surface')
 plt.xlabel('Maturity')
 plt.ylabel('Strike Price')
 plt.show()
 
-
 real_volatility_surface = load_data('data/real_volatility_surface.csv')
+
 performance_metrics = evaluate_model(real_volatility_surface, volatility_surface)
 print(performance_metrics)
-
